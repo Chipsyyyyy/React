f30d1da5984a8a205db227071104380e317132db
Chipsyyyyy <maxwell05.tran@gmail.com>
2026-06-08 00:26:45 +1000

    Modified states

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,13 +4,16 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      {/* <Counter/> */}
 
-      {/* <Greet  name ="Bruce" heroName="Batman">
+      {/* <Message/> */}
+
+      <Greet  name ="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet  name ="Clark" heroName="Superman">
@@ -18,7 +21,7 @@ function App() {
         </Greet>
       <Greet  name ="Diana" heroName="Wonder Woman"/>
 
-      <Welcome name="Bruce" heroName="Batman"/>
+      {/* <Welcome name="Bruce" heroName="Batman"/>
       <Welcome name="Clark" heroName="Superman"/>
       <Welcome name="Diana" heroName="Wonder Woman"/> */}
 
