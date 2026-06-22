3b4f9f024c1a8852c033a06b040b422f9bed138c
Chipsyyyyy <maxwell05.tran@gmail.com>
2026-06-23 00:57:41 +1000

    Learned event handling and event binding

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,21 +5,28 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
+     <EventBind/>
 
+     {/* <FunctionClick/>
+     <ClassClick/> */}
+
+      {/* <Counter/> */}
       {/* <Message/> */}
 
-      <Greet  name ="Bruce" heroName="Batman">
+      {/* <Greet  name ="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet  name ="Clark" heroName="Superman">
         <button>Action</button>
         </Greet>
-      <Greet  name ="Diana" heroName="Wonder Woman"/>
+      <Greet  name ="Diana" heroName="Wonder Woman"/> */}
 
       {/* <Welcome name="Bruce" heroName="Batman"/>
       <Welcome name="Clark" heroName="Superman"/>
