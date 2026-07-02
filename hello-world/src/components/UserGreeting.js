4bdb191b2f228df04806ca841a3c5269c925fdf6
Chipsyyyyy <maxwell05.tran@gmail.com>
2026-07-02 14:07:33 +1000

    Learned about conditional rendering, passing methods as props and list rendering

---
+++ hello-world/src/components/UserGreeting.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+export class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+
+    render() {
+
+        return this.state.isLoggedIn && <div>Welcome Maxwell</div>
+
+        // return(
+        //     this.state.isLoggedIn ? 
+        //     <div>Welcome Maxwell</div> :
+        //     <div>Welcome Guest</div>
+        // )
+
+        // let message
+        // if (this.state.isLoggedIn){
+        //     message = <div>Welcome Maxwell</div>
+        // } else {
+        //     message = <div>Welcome Guest</div>
+        // }
+
+        // return <div>{message}</div>
+
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>Welcome Maxwell</div>
+        //     )
+        // } else {
+        //         return <div>Welcome Guest</div>
+        //     }
+    //     return 
+    //     <div>
+    //         <div>Welcome Maxwell</div>
+    //         <div>Welcome Guest</div>
+    //     </div>
+    }
+}
+
+export default UserGreeting;
